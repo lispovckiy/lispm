015c56083cf8c1a64c42b47149f8aec6af7226dd
lispovckiy <devxtx@gmail.com>
2026-09-22 22:25:24 +1000

    refactoring: lispmc.c now readable

--- src/lispmc.c
+++ src/lispmc.c
@@ -2,42 +2,84 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+typedef struct {
+    const char* name;
+    int min_arguments;
+    const char *atom_name;
+    void (*handler)(XEvent *ev, int argc, char **argv);
+} Commands;
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) return 1;
-    Display *display = XOpenDisplay(NULL);
-    if (!display) return 1;
+void
+handle_view_move(XEvent *ev, int argc, char **argv);
+void
+handle_resize(XEvent *ev, int argc, char **argv);
 
-    Window root = DefaultRootWindow(display);
-    XEvent ev;
-    memset(&ev, 0, sizeof(ev));
+Commands cmds[] = {
+    { "close",      2, "CLOSE_WINDOW",             NULL             },
+    { "fullscreen", 2, "_NET_WM_STATE_FULLSCREEN",  NULL             },
+    { "view",       3, "CHANGE_WORKSPACE",         handle_view_move },
+    { "move",       3, "MOVE_WINDOW",              handle_view_move },
+    { "resize",     3, "RESIZE_WINDOW",            handle_resize    }
+};
 
-    ev.xclient.type = ClientMessage;
-    ev.xclient.window = root;
-    ev.xclient.format = 32;
-
-    if (strcmp(argv[1], "close") == 0) {
-        ev.xclient.message_type = XInternAtom(display, "CLOSE_WINDOW", False);
-    } else if (argc >= 3 && strcmp(argv[1], "view") == 0) {
-        ev.xclient.data.l[0] = atol(argv[2]);
-        ev.xclient.message_type = XInternAtom(display, "CHANGE_WORKSPACE", False);
-    } else if (argc >= 3 && strcmp(argv[1], "move") == 0) {
-        ev.xclient.data.l[0] = atol(argv[2]);
-        ev.xclient.message_type = XInternAtom(display, "MOVE_WINDOW", False);
-    } else if (strcmp(argv[1], "resize") == 0 && argc >= 3) {
-	float delta = atof(argv[2]);
-	ev.xclient.data.l[0] = (long)(delta * 100);
-	ev.xclient.message_type = XInternAtom(display, "RESIZE_WINDOW", False);
-    } else if (strcmp(argv[1], "fullscreen") == 0) {
-        ev.xclient.data.l[0] = 0;
-        ev.xclient.message_type = XInternAtom(display, "_NET_WM_STATE_FULLSCREEN", False);
-    } else {
-	    XCloseDisplay(display);
-	    return 1;
+static Display*
+OpenDisplay(void)
+{
+    Display *dis = XOpenDisplay(NULL);
+    if (!dis) {
+        fprintf(stderr, "lispmc: Cannot Open Display");
+        exit(1);
     }
 
-    XSendEvent(display, root, False, SubstructureNotifyMask, &ev);
-    XFlush(display);
-    XCloseDisplay(display);
+    return dis;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) { /* TODO: make a help */ return 1; }
+    Display *dis = OpenDisplay();
+    Window root = DefaultRootWindow(dis);
+    int num_cmds = sizeof(cmds) / sizeof(cmds[0]);
+    bool found = false;
+
+    for (int i = 0; i < num_cmds; i++) {
+        if (strcmp(argv[1], cmds[i].name) == 0 ){
+            found  = true;
+
+            if (argc < cmds[i].min_arguments) { XCloseDisplay(dis); exit(1);}
+
+            XEvent ev;
+            memset(&ev, 0, sizeof(ev));
+            ev.xclient.type = ClientMessage;
+            ev.xclient.window = root;
+            ev.xclient.format = 32;
+            ev.xclient.message_type = XInternAtom(dis, cmds[i].atom_name, False);
+
+            if (cmds[i].handler != NULL) {
+                cmds[i].handler(&ev, argc, argv);
+            }
+
+            XSendEvent(dis, root, False, SubstructureNotifyMask, &ev);
+            XFlush(dis);
+            break;
+        }
+    }
+
+    if (!found) {
+        XCloseDisplay(dis);
+        return 1;
+    }
+
+    XCloseDisplay(dis);
     return 0;
+}
+
+
+void handle_view_move(XEvent *ev, int argc, char **argv) {
+    ev->xclient.data.l[0] = atol(argv[2]);
+}
+
+void handle_resize(XEvent *ev, int argc, char **argv) {
+    float delta = atof(argv[2]);
+    ev->xclient.data.l[0] = (long)(delta * 100);
 }
